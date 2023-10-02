86ba800d2b44d440f2716dfdbad4e81e2bf6f200
Parthei <146698466+Parthei@users.noreply.github.com>
2023-10-02 11:15:00 +0530

    Update WeChartTest1.py

--- WeChartTest1.py
+++ WeChartTest1.py
@@ -55,6 +55,64 @@ if __name__=="__main__":
     ctrlV()
     time.sleep(1) #别问我为什么要停1秒，问就是给微信一个反应的时间，他反应慢反应不过来，其他位置暂停的原因同样
     enter()
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>To-Do List</title>
+</head>
+<body>
+    <h1>To-Do List</h1>
+    
+    <!-- Form to add a new task -->
+    <form id="task-form">
+        <label for="task">Add Task:</label>
+        <input type="text" id="task" required>
+        <button type="button" onclick="addTask()">Add</button>
+    </form>
+
+    <!-- Display the task list -->
+    <ul id="task-list"></ul>
+
+    <script>
+        // Function to add a new task
+        function addTask() {
+            var taskInput = document.getElementById("task");
+            var taskText = taskInput.value.trim();
+            if (taskText === "") return; // Ignore empty tasks
+
+            var taskList = document.getElementById("task-list");
+            var taskItem = document.createElement("li");
+
+            var checkBox = document.createElement("input");
+            checkBox.type = "checkbox";
+            checkBox.addEventListener("change", function() {
+                if (this.checked) {
+                    taskItem.style.textDecoration = "line-through";
+                } else {
+                    taskItem.style.textDecoration = "none";
+                }
+            });
+
+            var taskTextElement = document.createElement("span");
+            taskTextElement.textContent = taskText;
+
+            var deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.addEventListener("click", function() {
+                taskList.removeChild(taskItem);
+            });
+
+            taskItem.appendChild(checkBox);
+            taskItem.appendChild(taskTextElement);
+            taskItem.appendChild(deleteButton);
+
+            taskList.appendChild(taskItem);
+            taskInput.value = "";
+        }
+    </script>
+</body>
+</html>
+
     time.sleep(1)
     #3.复制要发送的消息，发送
     setText('你好啊')
